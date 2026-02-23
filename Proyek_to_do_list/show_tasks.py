8633b9bb58ec0b2a17b6e3173b3a26dbfc05362b
KhF4r <khalifahfarras03@gmail.com>
2026-02-23 16:31:36 +0700

    push module show_tasks

---
+++ Proyek_to_do_list/show_tasks.py
@@ -0,0 +1,11 @@
+def show_tasks(tasks):
+    if not tasks:
+        print("Belum ada tugas yang ditambahkan.")
+    for i, task in enumerate(tasks):
+
+        print(f"Tugas {i+1} ")
+        print(f"Judul Tugas : {task['title']}")
+        print(f"Status Tugas : {task['done']}")
+        print(f"Tanggal ditambahkan : {task['created_at']}")
+        print(f"Deadline : {task['deadline']}")
+        
