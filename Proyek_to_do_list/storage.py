eba375e460925b29a222c6f2fd260d34c7316618
Med <ahmedadam0050@gmail.com>
2026-02-23 16:46:13 +0700

    Push module storage.py dan tasks.json

---
+++ Proyek_to_do_list/storage.py
@@ -0,0 +1,14 @@
+# Menyimpan dan mengambil data dari json
+
+import json
+
+def load_tasks():
+    try:
+        with open("tasks.json", "r") as f:
+            return json.load(f)
+    except:
+        return []
+
+def save_tasks(tasks):
+    with open("tasks.json", "w") as f:
+        json.dump(tasks, f)
